edb373443fc61069f8f803ca74c3edc62dc6ccaa
Nyu Boo <yajanaranjomey@gmail.com>
2024-02-08 12:39:01 +0100

    Completed

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ I add a compiler script to test its function without having to do a make each ti
 - Does 'make'.
 - Compiles (with flags) your ft_printf function, the libftprintf.a and the main function.
 - Call the generated ./a.out and print the results.
+
+# Careful
+In case your main is inside your ft_printf.c, you should remove it from the script.
--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 /* My main does not follow the norme.
  * In case you want to test the whole ft_printf, you should uncomment the
- * variables
+ * variables and change the values as many times as you want!
  */
 
 #include <stdio.h>
 #include "ft_printf.h"
+
 int main()
 {
 	char	c = 'H';
-	char	*str = "42Barcelona";
+/*	char	*str = "42Barcelona";
 	int		p = 14523;
 	int		i = 0;
 	int		digit = 0;
@@ -16,19 +17,17 @@ int main()
 	unsigned int		X = 4294967286;
 	unsigned int		u = 4294967295;
 	int		*ptr = &p;
-
+*/
 	int org = 0;
 	int mine = 0;
 
-	org = printf("---Org---\nChar: %c\nStr: %s\nPtr: %p\nInt: %i\nDig: %d\nHex_low: %x\nHex_up: %X\nUns: %u\nPercent: %%\n", c, str, (void*)ptr, i, digit, x, X, u);
+/*	org = printf("---Org---\nChar: %c\nStr: %s\nPtr: %p\nInt: %i\nDig: %d\nHex_low: %x\nHex_up: %X\nUns: %u\nPercent: %%\n", c, str, (void*)ptr, i, digit, x, X, u);
 	mine = ft_printf("---Mine---\nChar: %c\nStr: %s\nPtr:%p\nInt: %i\nDig: %d\nHex_low: %x\nHex_up: %X\nUns: %u\nPercent: %%\n", c, str, (void*)ptr, i, digit, x, X, u );
+*/
+	org = printf("O:%s\n", c);
+	mine = ft_printf("M:%s\n", c);
 
-//	org = printf("O:%s\n", c);
-//	mine = ft_printf("M:%s\n", c);
-//	org = printf("O:%p\n", (void *)-14523);
-//	mine = ft_printf("M:%p\n", (void *)-14523);
-
-	printf("res:%d\n%d\n", org, mine);
+	printf("res:%d\nres:%d\n", org, mine);
 
 	return (0);
 }
